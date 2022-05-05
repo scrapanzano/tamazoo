ed300997d7d12d6e8c98edbf871266b0845b96cd
Scrapa <d.leone001@studenti.unibs.it>
2022-05-05 20:21:28 +0200

    Initial commit

---
+++ src/it/unibs/fp/tamazoo/TamaMain.java
@@ -0,0 +1,10 @@
+package it.unibs.fp.tamazoo;
+
+public class TamaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/it/unibs/fp/tamazoo/Tamagotchi.java
@@ -0,0 +1,5 @@
+package it.unibs.fp.tamazoo;
+
+public class Tamagotchi {
+
+}
